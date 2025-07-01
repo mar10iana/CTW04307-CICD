56211fec51538080919a07950b903edf93346ec8
mariana <qxz60rn>
2025-07-01 10:37:28 +0100

    runs

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+//TODO something is missing here :)
+export const TOOL_URL_PATTERN = `https://github.com/mikefarah/yq/releases/download/v[VERSION]/yq_linux_[ARCH]`;
+//TODO something is missing here :)
+export const TOOL_NAME = `yq`;
+
+export const TOOL_DOWNLOAD_SOURCE = `YQ CLI`;
