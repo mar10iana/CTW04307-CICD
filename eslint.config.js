763e4354bb96bbebb08b0ebcf8cfc34f472a6cff
mariana <qxz60rn>
2025-07-02 15:02:30 +0100

    1

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,27 @@
-import eslintPluginJs from '@eslint/js';
+import js from '@eslint/js';
+import * as eslintPluginNode from 'eslint-plugin-node';
+import globals from 'globals';
 
 /** @type {import("eslint").Linter.FlatConfig} */
 export default [
-  eslintPluginJs.configs.recommended,
+  {
+    ignores: ['node_modules/**'],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      node: eslintPluginNode,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
 ];
